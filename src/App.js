2c934cc49a3ce9a3914094086b2c1858349d6be1
Reynaldo <reynaldoqs@gmail.com>
2020-04-24 11:55:33 -0400

    add README file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Router } from "@reach/router";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "./styles";
 
 import Home from "./pages/Home";
-import Detail from "./pages/Detail";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Router>
-          <Home path="/" />
-          <Detail path="/detail" />
-        </Router>
+        <Home />
       </ThemeProvider>
     </>
   );
